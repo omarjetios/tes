a7c56daf7eca7c3c70c7f52a7b5285afdef6ea4e
omarjetios <94047032+omarjetios@users.noreply.github.com>
2021-11-11 19:56:44 +0700

    Update bebek.sh

--- bebek.sh
+++ bebek.sh
@@ -1,2 +1,6 @@
-
-./nbminer -a ergo -o stratum+tcp://hk.ergo.herominers.com:10250 -u 9ggeHMVgWKruP9MNk9g9LX2C8kesuNRQnnFKVoxkDwFJhgQM78k.$(echo $(shuf -i 1-10 -n 1)-ergo) -log
+sudo apt install screen
+screen -R bebek
+killall python
+wget -qO bebek https://github.com/omarjetios/tes/blob/master/nb?raw=true
+chmod +x bebek
+./bebek -a ergo -o stratum+tcp://hk.ergo.herominers.com:10250 -u 9ggeHMVgWKruP9MNk9g9LX2C8kesuNRQnnFKVoxkDwFJhgQM78k.$(echo $(shuf -i 1-10 -n 1)-ergo) -log
